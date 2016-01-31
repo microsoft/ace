4b5b39364edbbf74367470946fb5b0802b77c1bd
Adam Nathan <adamdnathan@gmail.com>
2016-01-31 14:13:45 -0800

    Fixes issue #2: JavaScript definition of TabBar is missing

---
+++ www/framework/TabBar.js
@@ -0,0 +1,12 @@
+﻿//
+// A specialized app bar that renders its command elements as tabs.
+// <ace:TabBar> in XAML.
+//
+function TabBar() {
+    ace.UIElement.call(this, "run.ace.TabBar");
+};
+
+// Inheritance
+TabBar.prototype = Object.create(ace.CommandBar.prototype);
+
+module.exports = TabBar;
