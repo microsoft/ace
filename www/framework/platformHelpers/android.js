ec4e4c77a89949bf40fa533b15e5a0ef72730483
Adam Nathan <adamdnathan@gmail.com>
2016-02-25 12:11:54 -0800

    Android version check helper for JavaScript

--- www/framework/platformHelpers/android.js
+++ www/framework/platformHelpers/android.js
@@ -11,6 +11,21 @@ var context = null;
 var activity = null;
 
 module.exports = {
+    ifVersionAtLeast: function(version, onTrue, onFalse) {
+        if (!onTrue) {
+            throw new Error("An onTrue callback must be specified.");
+        }
+
+        ace.NativeObject.getField("android.os.Build$VERSION", "SDK_INT", function (sdk_int) {
+            if (sdk_int >= version) {
+                onTrue(sdk_int);
+            }
+            else if (onFalse) {
+                onFalse(sdk_int);
+            }
+        });
+    },
+    
     getContext: function () {
         if (!context)
             context = new ace.KnownNativeObject("android.content.Context");
@@ -40,5 +55,32 @@ module.exports = {
         add: function(text) {
             ace.NativeObject.invoke("run.ace.AppWidgetData", "add", text, ace.android.getContext());
         }
+    },
+    
+    version: {
+        BASE: 1,
+        BASE_1_1: 2,
+        CUPCAKE: 3,
+        CUR_DEVELOPMENT: 10000,
+        DONUT: 4,
+        ECLAIR: 5,
+        ECLAIR_0_1: 6,
+        ECLAIR_MR1: 7,
+        FROYO: 8,
+        GINGERBREAD: 9,
+        GINGERBREAD_MR1: 10,
+        HONEYCOMB: 11,
+        HONEYCOMB_MR1: 12,
+        HONEYCOMB_MR2: 13,
+        ICE_CREAM_SANDWICH: 14,
+        ICE_CREAM_SANDWICH_MR1: 15,
+        JELLY_BEAN: 16,
+        JELLY_BEAN_MR1: 17,
+        JELLY_BEAN_MR2: 18,
+        KITKAT: 19,
+        KITKAT_WATCH: 20,
+        LOLLIPOP: 21,
+        LOLLIPOP_MR1: 22,
+        M: 23
     }
 };
